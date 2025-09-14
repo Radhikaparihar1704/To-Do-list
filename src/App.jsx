bd16c104f6217f789e42eb23291e7443a1d76808
Radhikaparihar1704 <radhikaparihar1704@gmail.com>
2025-09-14 17:34:50 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import Todo from "./components/Todo"
 
 function App() {
   return (
     <>
-      <div className='w-full h-screen bg-zinc-200 flex justify-center items-center'> 
+      <div className='w-full h-screen flex justify-center items-center'> 
       <Todo/>
       </div>
     </>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,75 @@
 import React, { useState } from 'react'
 
-function Todo() {
-    const[val ,setVal] = useState("");
-    const[ todo,setTodo]=useState([])
-    const buttonhandler= ()=>{
-        if (val.trim() === "") return;
-        setTodo([...todo,val]);
-        setVal("");   
-    }
-    return (
-    <div className='w-container max-auto  h-[70vh]  bg-gray-500  rounded-md flex  flex-col'>
-      <h1 className='flex justify-center font-bold text-3xl m-5  underline'>To-Do List</h1>
-        <div className=" w-full  ">
-        <input className="rounded-md m-2 border border-black-700 h-8 p-2 text-white " onChange={(e)=>setVal(e.target.value)} value={val} type="text" placeholder="Enter your task"/>
-        <button onClick={buttonhandler} className=' h-10 w-20 bg-black text-white rounded-full m-3 px-4 py-2  border border-white '>Add +</button>
-        <ul className='mt-1'>
-         {todo.map((item ,index)=>(
-          <li className='bg-white h-8 m-2 flex items-center p-2 rounded-md text-black border border-black-600 flex justify-between'>
-          {item}
-          <button onClick={()=> setTodo(todo.filter((item,id)=> index!==id))} className=' bg-black text-white rounded-full px-2 border border-white'>Delete</button>
-         </li>
-         ))}
-          </ul>
+function Todopractice() {
+   const[val,setval] =useState("");
+   const[task,settask]=useState([]);
+   const[edittask ,setedittask]=useState(null);
+//add button
+ const buttonhandler= ()=> {
+   if (val.trim() === "") return;
+      if (edittask !== null) {
+         const updated = [...task];
+         updated[edittask].text = val;
+         settask(updated);
+         setedittask(null);
+      } else {
+         
+         settask([...task, { text: val, onactive: false }]);
+      }
+      setval("");
+   
+  }
+
+ //remove button
+
+const  removebutton= (i)=>{
+    settask(task.filter((item,index)=> index!==i))
+}
+//Edit button 
+
+const edit =(index)=>{
+  setedittask(index);
+  setval(task[index].text);
+};
+
+
+//checkbox
+const checkbox = (i)=>{
+  settask(task.map((item,index)=>
+    index==i ?{...item , onactive:!item.onactive }:item
+  ))
+
+
+};
+
+  return (
+    <div className='h-screen w-[50vw] text-center '>
+         <h1 className='mt-10 text-5xl'>TO-DO List</h1>
+         <div className='w-full flex items-center justify-between mt-10'>
+         <input onChange={(e)=> setval(e.target.value)}  className='px-6  py-2 border-2 border-black-600 rounded-md text-lg 'placeholder='Enter a Task ' type="text" value={val}/>
+         <button onClick={()=>buttonhandler()} className="bg-green-300 h-12 w-26 rounded-full mr-5 "> ADD+</button>
+         </div>
+         <ul  className= "mt-10">
+           {task.map((item, i) => (
+            <li className ={`h-10 rounded-md ${item.onactive ?"bg-green-300" :"bg-white"}  border border-black-200 text-lg mb-3 flex justify-between px-5 py-1`}>
+             <input onClick={()=>checkbox(i)} checked= {item.onactive}type="checkbox" />
+             {item.text}
+             <div>
+               {item.onactive ? (
+              <span className="text-green-700 font-semibold">Completed..!</span>
+              ) : (
+             <>
+             <button onClick={()=>edit(i)} className='bg-yellow-400 h-full rounded-full px-2 '>Edit</button>
+             <button onClick={()=>removebutton(i)} className='bg-red-400 h-full rounded-full px-2 ml-4'>Delete</button>
+             </>
+        )}
         </div>
+            </li>
+           ))}
+         </ul>
     </div>
   )
 }
 
-export default Todo
+export default Todopractice;
+
